3865b8679922755b8468c2c50ff942ab277b8269
Jeremy Zoitas <jezz331@gmail.com>
2018-07-29 16:44:41 +1000

    ---Working Camera--- Got Camera working with collisions. Consits of three scripts one that sets hidden object materials back, another which moves the camera, and lastly one which does the collision calculations.The collision and hide objects script go on the main camera and the camera follow goes on a gameobject wich is the parent of the main camera.

---
+++ Assets/Scripts/CameraCollision2.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision2 : MonoBehaviour {
+
+	public float minDistance = 1.0f;
+	public float maxDistance = 4.0f;
+	public float smooth = 10.0f;
+	Vector3 dollyDir;
+	public Vector3 dollyDirAdjusted;
+	public float distance;
+	private float m_fChangeTime = 1.0f;
+
+	// Use this for initialization
+	void Awake () {
+		dollyDir = transform.localPosition.normalized;
+		distance = transform.localPosition.magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 desiredCameraPos = transform.parent.TransformPoint (dollyDir * maxDistance);
+		RaycastHit hit;
+		RaycastHit Hit;
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		m_fChangeTime -= Time.deltaTime;
+
+		if (m_fChangeTime <= 0.5f)
+		{
+			m_fChangeTime = 0.5f;
+		}
+
+		if (Physics.Raycast(transform.position, forward, out Hit, distance - 0.8f))
+		{
+			if (Hit.collider.gameObject.layer == 8)
+			{
+				Hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.5f, 0.5f, 0.5f, m_fChangeTime);
+			}
+		}
+		else
+		{
+			m_fChangeTime = 1.0f;
+		}
+		Debug.DrawLine(transform.parent.position, desiredCameraPos, Color.red);
+
+		if (Physics.Linecast (transform.parent.position, desiredCameraPos, out hit))
+		{
+			if(hit.collider.gameObject.layer != 8)
+			{
+				distance = Mathf.Clamp ((hit.distance * 0.87f), minDistance, maxDistance);
+			}
+		}
+		else
+		{
+			distance = maxDistance;
+		}
+
+		transform.localPosition = Vector3.Lerp (transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+	}
+}
