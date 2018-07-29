ac0ddf6cebfab583853ec2957911b04743703fae
Jeremy Zoitas <jezz331@gmail.com>
2018-07-30 09:57:43 +1000

    ---Reset Buton Fix--- Fixed Reset Button.

--- Assets/Scripts/CameraFollow2.cs
+++ Assets/Scripts/CameraFollow2.cs
@@ -1,34 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
 
 public class CameraFollow2 : MonoBehaviour {
 
 	public float CameraMoveSpeed = 120.0f;
 	public GameObject CameraFollowObj;
-	Vector3 FollowPOS;
 	public float MaxClampAngle = 80.0f;
 	public float MinClampAngle = -45.0f;
 	public float inputSensitivity = 150.0f;
-	public GameObject CameraObj;
-	public GameObject PlayerObj;
-	public float camDistanceXToPlayer;
-	public float camDistanceYToPlayer;
-	public float camDistanceZToPlayer;
 	public float mouseX;
 	public float mouseY;
 	public float finalInputX;
 	public float finalInputZ;
-	public float smoothX;
-	public float smoothY;
 	private float rotY = 0.0f;
 	private float rotX = 0.0f;
-
-
 
 	// Use this for initialization
 	void Start () {
 		Vector3 rot = transform.localRotation.eulerAngles;
+		rot.x = 20.0f;
 		rotY = rot.y;
 		rotX = rot.x;
 		Cursor.lockState = CursorLockMode.Locked;
@@ -51,10 +43,14 @@ public class CameraFollow2 : MonoBehaviour {
 
 		rotX = Mathf.Clamp (rotX, MinClampAngle, MaxClampAngle);
 
+		if(XCI.GetButton(XboxButton.RightStick))
+		{
+			rotX = 20.0f;
+			rotY = CameraFollowObj.transform.rotation.eulerAngles.y;
+		}
+
 		Quaternion localRotation = Quaternion.Euler (rotX, rotY, 0.0f);
 		transform.rotation = localRotation;
-
-
 
 	}
 
