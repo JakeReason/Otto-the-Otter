66d91d63a5d0f50ccb3f7509a3aa722f0b554019
Matthew Le Nepveu <matthew@lenepveu.com>
2018-08-13 15:30:12 +1000

    Cleaned up my code and made some progress on implementing animations in the game.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -70,6 +70,8 @@ public class Player : MonoBehaviour
     // Private Vector3 stores the direction the player should move
     private Vector3 m_v3MoveDirection;
 
+	private Vector3 m_v3MoveVector;
+
     // Vector3 represents the direction the player will look in
     private Vector3 m_v3LookDirection;
 
@@ -97,9 +99,6 @@ public class Player : MonoBehaviour
     // Bool used to let the script know if the player has jumped from ground
     private bool m_bJumped;
 
-    // Private bool indicates if the player is currently jumping
-    private bool m_bJumping;
-
 	// Indicates if the player is recovering after a hit or not
     private bool m_bRecovering;
 
@@ -118,8 +117,9 @@ public class Player : MonoBehaviour
         m_animator = GetComponent<Animator>();
 
 		// Sets all bools in the animator controller to false
-        m_animator.SetBool("Walking", false);
-        m_animator.SetBool("Jumping", false);
+		m_animator.SetBool("Moving", false);
+		m_animator.SetBool("Grapple", false);
+		m_animator.SetBool("Jumping", false);
 		m_animator.SetBool("Falling", false);
 		m_animator.SetBool("Landing", false);
 
@@ -140,7 +140,8 @@ public class Player : MonoBehaviour
 
         // Initialises the Move, Look and Gravity Direction all to equal the zero Vector3
         m_v3MoveDirection = Vector3.zero;
-        m_v3LookDirection = Vector3.zero;
+		m_v3MoveVector = Vector3.zero;
+		m_v3LookDirection = Vector3.zero;
         m_v3Gravity = Vector3.zero;
 
         // Initialises private floats to equal zero
@@ -153,7 +154,6 @@ public class Player : MonoBehaviour
 
         // Sets private bools to false on awake
         m_bJumped = false;
-        m_bJumping = false;
         m_bRecovering = false;
     }
 
@@ -162,163 +162,205 @@ public class Player : MonoBehaviour
     //--------------------------------------------------------------------------------
     void Update()
     {
-        // Creates a new Vector3 indicating which direction the left stick is facing
-        m_v3MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,
-                                        Input.GetAxis("Vertical"));
+		Move();
+		Animate();
+	}
 
-        // Stores the x value of Move Direction into the float for use when jumping
-        m_fMovementX = m_v3MoveDirection.x;
+	private void Move()
+	{
+		// Creates a new Vector3 indicating which direction the left stick is facing
+		m_v3MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,
+										Input.GetAxis("Vertical"));
 
-        // Sets the look direction to equal the direction the control stick is facing
-        m_v3LookDirection = new Vector3(m_v3MoveDirection.x, 0, m_v3MoveDirection.z);
+		// Stores the x value of Move Direction into the float for use when jumping
+		m_fMovementX = m_v3MoveDirection.x;
 
-        // Checks if there is any movement from the control stick
-        if (m_v3LookDirection.x < 0.1f && m_v3LookDirection.z < 0.1f)
-        {
-            // Makes the look direction equal the previous frame's look direction
-            m_v3LookDirection = m_v3PreviousLook;
-        }
-        // Else if there is input from the control stick
-        else
-        {
-            // Stores the look direction to be the previous frame direction
-            m_v3PreviousLook = m_v3LookDirection;
-        }
+		// Sets the look direction to equal the direction the control stick is facing
+		m_v3LookDirection = new Vector3(m_v3MoveDirection.x, 0, m_v3MoveDirection.z);
 
-        // Detects if Grappling Hook is hooked on an object
-        if (m_grapplingScript.GetHooked())
-        {
-            // Sets gravity to equal the zero Vector3
-            m_v3Gravity = Vector3.zero;
-        }
-        // Else if the grappling hook hasn't hooked an object
-        else
-        {
-            // Checks if Jump is pressed, the player is grounded and if they haven't jumped
-            if (Input.GetButton("Jump") && m_cc.isGrounded && !m_bJumped)
-            {
-                // Sets the y value of gravity to equal the jump speed
-                m_v3Gravity.y = m_fJumpSpeed;
+		// Checks if there is any movement from the control stick
+		if (m_v3LookDirection.x < 0.1f && m_v3LookDirection.z < 0.1f)
+		{
+			// Makes the look direction equal the previous frame's look direction
+			m_v3LookDirection = m_v3PreviousLook;
+		}
+		// Else if there is input from the control stick
+		else
+		{
+			// Stores the look direction to be the previous frame direction
+			m_v3PreviousLook = m_v3LookDirection;
+		}
 
-                // Sets jumped and jumping bools to be true
-                m_bJumped = true;
-                m_bJumping = true;
+		// Detects if Grappling Hook is hooked on an object
+		if (m_grapplingScript.GetHooked())
+		{
+			m_animator.SetBool("Grapple", true);
+
+			// Sets gravity to equal the zero Vector3
+			m_v3Gravity = Vector3.zero;
+		}
+		// Else if the grappling hook hasn't hooked an object
+		else
+		{
+			m_animator.SetBool("Grapple", false);
+
+			// Checks if Jump is pressed, the player is grounded and if they haven't jumped
+			if (Input.GetButton("Jump") && m_cc.isGrounded && !m_bJumped)
+			{
+				// Sets the y value of gravity to equal the jump speed
+				m_v3Gravity.y = m_fJumpSpeed;
+
+				// Sets jumped bool to be true
+				m_bJumped = true;
 
 				// Sets Jumping bool to true and Landing bool to false in the animator
-                m_animator.SetBool("Jumping", true);
-                m_animator.SetBool("Landing", false);
-            }
-            // Else if Jump button isn't pressed and the player is grounded
-            else if (!Input.GetButton("Jump") && m_cc.isGrounded)
-            {
-                // Sets jumped and jumping bools back to false
-                m_bJumped = false;
-                m_bJumping = false;
-                m_fJumpTimer = 0.0f;
+				m_animator.SetBool("Jumping", true);
+				m_animator.SetBool("Landing", false);
+			}
+			// Else if Jump button isn't pressed and the player is grounded
+			else if (!Input.GetButton("Jump") && m_cc.isGrounded)
+			{
+				// Sets jumped and jumping bools back to false
+				//m_bJumped = false;
+				//m_fJumpTimer = 0.0f;
 
 				// Sets Landing bool to true and Jumping and falling bool to false in animator
-                m_animator.SetBool("Landing", true);
-                m_animator.SetBool("Falling", false);
-                m_animator.SetBool("Jumping", false);
-            }
-            // Else if Jump isn't pressed and player is in air or player has jumped too long
-            else if ((!Input.GetButton("Jump") && !m_cc.isGrounded) ||
-                      m_fJumpTimer > m_fJumpTimeLimit)
-            {
-                // Applies gravity to player with an extra multiplier to fall quicker
-                m_v3Gravity += Physics.gravity * m_fExtraGravity * Time.deltaTime;
-
-				// Sets Falling bool to true in the animator
-                m_animator.SetBool("Falling", true);
-            }
-            // Else if the player is falling
-            else if (!m_cc.isGrounded)
-            {
-                // Apply gravity to the player without miltiplier
-                m_v3Gravity += Physics.gravity * Time.deltaTime;
+				m_animator.SetBool("Landing", true);
+				m_animator.SetBool("Falling", false);
+				m_animator.SetBool("Jumping", false);
+			}
+			// Else if Jump isn't pressed and player is in air or player has jumped too long
+			else if ((!Input.GetButton("Jump") && !m_cc.isGrounded) ||
+					  m_fJumpTimer > m_fJumpTimeLimit)
+			{
+				// Applies gravity to player with an extra multiplier to fall quicker
+				m_v3Gravity += Physics.gravity * m_fExtraGravity * Time.deltaTime;
 
 				// Sets Falling bool to true in the animator
 				m_animator.SetBool("Falling", true);
-            }
-        }
+			}
+		}
 
-        // Checks if the player is in the air
-        if (m_bJumping)
-        {
-            // Adds the jump timer to deltaTime every second
-            m_fJumpTimer += Time.deltaTime;
-        }
+		// Checks if the player is in the air
+		if (m_bJumped)
+		{
+			// Adds the jump timer to deltaTime every second
+			m_fJumpTimer += Time.deltaTime;
+		}
 
-        // Stores the y movement direction in local float
-        float fCurrentMoveY = m_v3MoveDirection.y;
+		// Stores the y movement direction in local float
+		float fCurrentMoveY = m_v3MoveDirection.y;
 
 		// Calculates the move direction in camera space rather than world space
-        m_v3MoveDirection = Camera.main.transform.rotation * m_v3MoveDirection;
+		m_v3MoveDirection = Camera.main.transform.rotation * m_v3MoveDirection;
 
-        // Stores local float value as the y value for the Move Direction
-        m_v3MoveDirection.y = fCurrentMoveY;
+		// Stores local float value as the y value for the Move Direction
+		m_v3MoveDirection.y = fCurrentMoveY;
 
-        // Multiples Move Direction vector by speed
-        m_v3MoveDirection *= m_fSpeed;
+		// Multiples Move Direction vector by speed
+		m_v3MoveVector = m_v3MoveDirection *= m_fSpeed;
 
-        // Applies the gravity to the move direction
-        m_v3MoveDirection += m_v3Gravity;
+		// Applies the gravity to the move direction
+		m_v3MoveVector = m_v3MoveDirection + m_v3Gravity;
 
 		// Makes player stop moving if the Grappling Hook has been fired or hooked
-        if (m_grapplingScript.GetHooked() || m_grapplingScript.GetFired())
-        {
-            m_v3MoveDirection = Vector3.zero;
-        } 
+		if (m_grapplingScript.GetHooked() || m_grapplingScript.GetFired())
+		{
+			m_v3MoveDirection = Vector3.zero;
+		}
 
-        // Adds movement to CharacterController based on move direction and delta time
-        m_cc.Move(m_v3MoveDirection * Time.deltaTime);
+		// Adds movement to CharacterController based on move direction and delta time
+		m_cc.Move(m_v3MoveVector * Time.deltaTime);
 
 		// Checks if the magnitude of the move direction is greater than 0.1
-        if (m_v3MoveDirection.sqrMagnitude > 0.1f)
-        {
-			// Sets Walking bool to true in the animator for the player
-            m_animator.SetBool("Walking", true);
-
+		if (m_v3MoveDirection.sqrMagnitude > 0.1f)
+		{
 			// Calculates the look direction by adding the position with the move direction
-            m_v3LookDirection = transform.position + m_v3MoveDirection.normalized;
+			m_v3LookDirection = transform.position + m_v3MoveDirection.normalized;
 
 			// Sets the y value of look direction to equal the player's y position
-            m_v3LookDirection.y = transform.position.y;
+			m_v3LookDirection.y = transform.position.y;
 
 			// Makes the player face the direction that they are walking
 			transform.LookAt(m_v3LookDirection, Vector3.up);
-        }
-        else
-        {
-            m_animator.SetBool("Walking", false);
-        }
+		}
 
-        if (m_bRecovering)
-        {
-            m_fHealthTimer += Time.deltaTime;
+		if (m_bRecovering)
+		{
+			m_fHealthTimer += Time.deltaTime;
 
-            if (Mathf.Sin(m_fHealthTimer / m_fFlashingRate) >= 0)
-            {
-                m_meshRenderer.material.color = m_flashColour;
-            }
-            else
-            {
-                m_meshRenderer.material.color = m_originalColour;
-            }
+			if (Mathf.Sin(m_fHealthTimer / m_fFlashingRate) >= 0)
+			{
+				m_meshRenderer.material.color = m_flashColour;
+			}
+			else
+			{
+				m_meshRenderer.material.color = m_originalColour;
+			}
 
-            if (m_fHealthTimer >= m_fHealthRecoveryTime)
-            {
-                m_fHealthTimer = 0.0f;
-                m_bRecovering = false;
-                m_meshRenderer.material.color = m_originalColour;
-            }
-        }
+			if (m_fHealthTimer >= m_fHealthRecoveryTime)
+			{
+				m_fHealthTimer = 0.0f;
+				m_bRecovering = false;
+				m_meshRenderer.material.color = m_originalColour;
+			}
+		}
 	}
 
-    //--------------------------------------------------------------------------------
-    // Function deducts health from the player when called.
-    //--------------------------------------------------------------------------------
-    public void Damage()
+	private void Animate()
+	{
+		if (m_bJumped && m_v3Gravity.y == m_fJumpSpeed)
+		{
+			m_animator.SetBool("Jumping", true);
+		}
+		else
+		{
+			m_animator.SetBool("Jumping", false);
+		}
+
+		if (m_bJumped && m_v3Gravity.y != m_fJumpSpeed)
+		{
+			m_animator.SetBool("Falling", true);
+		}
+		else
+		{
+			m_animator.SetBool("Falling", false);
+		}
+
+		if (m_bJumped && m_cc.isGrounded)
+		{
+			m_animator.SetBool("Landing", true);
+			m_bJumped = false;
+			m_fJumpTimer = 0.0f;
+		}
+		else
+		{
+			m_animator.SetBool("Landing", false);
+		}
+
+		if (m_grapplingScript.GetHooked() || m_grapplingScript.GetFired())
+		{
+			m_animator.SetBool("Grapple", true);
+		}
+		else
+		{
+			m_animator.SetBool("Grapple", false);
+		}
+
+		if (m_v3MoveDirection.sqrMagnitude > 0.1f)
+		{
+			m_animator.SetBool("Moving", true);
+		}
+		else
+		{
+			m_animator.SetBool("Moving", false);
+		}
+	}
+
+	//--------------------------------------------------------------------------------
+	// Function deducts health from the player when called.
+	//--------------------------------------------------------------------------------
+	public void Damage()
     {
         if (m_fHealthTimer <= 0.0f || !m_bRecovering)
         {
