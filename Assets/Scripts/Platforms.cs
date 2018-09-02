0adc3b0fae429c5a958d7353e6eca2a0650ae42a
Jeremy Zoitas <jezz331@gmail.com>
2018-09-03 09:53:07 +1000

    ---Updated platforms--- Added transform array, so the platform can move to them and go back.

--- Assets/Scripts/Platforms.cs
+++ Assets/Scripts/Platforms.cs
@@ -4,38 +4,94 @@ using UnityEngine;
 
 public class Platforms : MonoBehaviour
 {
+	[SerializeField]
+	// 
+	private bool m_bOrbit = false;
 
-	public bool m_bOrbit = false;
-	public bool m_bMoveUp = false;
-	public bool m_bMoveDown = false;
-	public bool m_bMoveLeft = false;
-	public bool m_bMoveRight = false;
-	public bool m_bMoveForward = false;
-	public bool m_bMoveBackward = false;
-	public bool m_bRotateUp = false;
-	public bool m_bRotateDown = false;
-	public bool m_bRotateLeft = false;
-	public bool m_bRotateRight = false;
-	public bool m_bRotateForward = false;
-	public bool m_bRotateBackward = false;
-	public bool m_bFollowPath = false;
-	public float m_fSpeed = 1;
-	public float m_fRotationSpeed = 10;
-	private float m_fMoveTimer = 0;
-	public float m_fReturnTime = 3;
-	public float m_fWaitTime = 1;
-	public float m_fOriginalTime = 1;
-	private float m_fResetTime;
+	[SerializeField]
+	// 
+	private bool m_bMoveUp = false;
+
+	[SerializeField]
+	// 
+	private bool m_bMoveDown = false;
+
+	[SerializeField]
+	// 
+	private bool m_bMoveLeft = false;
+
+	[SerializeField]
+	// 
+	private bool m_bMoveRight = false;
+
+	[SerializeField]
+	// 
+	private bool m_bMoveForward = false;
+
+	[SerializeField]
+	// 
+	private bool m_bMoveBackward = false;
+
+	[SerializeField]
+	// 
+	private bool m_bRotateUp = false;
+
+	[SerializeField]
+	// 
+	private bool m_bRotateDown = false;
+
+	[SerializeField]
+	// 
+	private bool m_bRotateLeft = false;
+
+	[SerializeField]
+	// 
+	private bool m_bRotateRight = false;
+
+	[SerializeField]
+	// 
+	private bool m_bRotateForward = false;
+
+	[SerializeField]
+	// 
+	private bool m_bRotateBackward = false;
+
+	[SerializeField]
+	// 
+	private bool m_bFollowPath = false;
+
+	[SerializeField]
+	// 
+	private float m_fSpeed = 1;
+
+	[SerializeField]
+	// 
+	private float m_fRotationSpeed = 10;
+
+	[SerializeField]
+	// 
+	private float m_fReturnTime = 3;
+
+	[SerializeField]
+	// 
+	private float m_fWaitTime = 1;
 
 	[SerializeField]
 	// An array of transform used to create a patrol route.
 	private Transform[] m_targetPoints;
 
-	[SerializeField]
+	//
+	private float m_fMoveTimer = 0;
+
+	//
+	private float m_fOriginalTime = 1;
+
+	//
+	private float m_fResetTime;
+
 	// Keeps track of the current waypoint number.
 	private int m_nDestPoint = 0;
 
-	[SerializeField]
 	// Used to keep track of whether the enemy of going backwards. 
 	private bool m_bGoBackWards = false;
 
@@ -44,8 +100,7 @@ public class Platforms : MonoBehaviour
 	{
 		m_fResetTime += m_fReturnTime * 2 + m_fWaitTime * m_fWaitTime;
 		m_fWaitTime += m_fReturnTime;
-		// Sets the first point to go to.
-		GoToNextPoint();
+		m_fOriginalTime = m_fWaitTime;
 	}
 
 	// Update is called once per frame
@@ -54,6 +109,7 @@ public class Platforms : MonoBehaviour
 		m_fMoveTimer += Time.deltaTime;
 		Move();
 		Rotate();
+		Orbit();
 		FollowPath();
 	}
 
@@ -215,45 +271,43 @@ public class Platforms : MonoBehaviour
 		}
 		if (m_bMoveRight)
 		{
-			if (m_bMoveUp)
+			if (m_fMoveTimer < m_fReturnTime && m_fMoveTimer > m_fOriginalTime)
 			{
-				if (m_fMoveTimer < m_fReturnTime && m_fMoveTimer > m_fOriginalTime)
-				{
-					transform.position += transform.right * m_fSpeed * Time.deltaTime;
-				}
-				if (m_fMoveTimer > m_fWaitTime)
-				{
-					transform.position -= transform.right * m_fSpeed * Time.deltaTime;
-				}
-				if (m_fMoveTimer >= m_fResetTime)
-				{
-					m_fMoveTimer = 0;
-				}
+				transform.position += transform.right * m_fSpeed * Time.deltaTime;
+			}
+			if (m_fMoveTimer > m_fWaitTime)
+			{
+				transform.position -= transform.right * m_fSpeed * Time.deltaTime;
+			}
+			if (m_fMoveTimer >= m_fResetTime)
+			{
+				m_fMoveTimer = 0;
 			}
 		}
 		if (m_bMoveLeft)
 		{
-			if (m_bMoveUp)
+			if (m_fMoveTimer < m_fReturnTime && m_fMoveTimer > m_fOriginalTime)
 			{
-				if (m_fMoveTimer < m_fReturnTime && m_fMoveTimer > m_fOriginalTime)
-				{
-					transform.position -= transform.right * m_fSpeed * Time.deltaTime;
-				}
-				if (m_fMoveTimer > m_fWaitTime)
-				{
-					transform.position += transform.right * m_fSpeed * Time.deltaTime;
-				}
-				if (m_fMoveTimer >= m_fResetTime)
-				{
-					m_fMoveTimer = 0;
-				}
+				transform.position -= transform.right * m_fSpeed * Time.deltaTime;
+			}
+			if (m_fMoveTimer > m_fWaitTime)
+			{
+				transform.position += transform.right * m_fSpeed * Time.deltaTime;
+			}
+			if (m_fMoveTimer >= m_fResetTime)
+			{
+				m_fMoveTimer = 0;
 			}
 		}
 	}
 
 	public void Orbit()
 	{
-
+		if(m_bOrbit)
+		{
+			transform.position += transform.right * m_fSpeed * Time.deltaTime;
+			transform.Rotate(Vector3.up * (m_fRotationSpeed * Time.deltaTime));
+		}
 	}
 
 	//--------------------------------------------------------------------------------
@@ -264,9 +318,6 @@ public class Platforms : MonoBehaviour
 		// Returns if no points have been set up
 		if (m_targetPoints.Length == 0)
 			return;
-
-		// Set the agent to go to the currently selected destination.
-		transform.position = Vector3.MoveTowards(transform.position, m_targetPoints[m_nDestPoint].position, m_fSpeed * Time.deltaTime);
 
 		// Choose the next point in the array as the destination,
 		// cycling to the start if necessary.
@@ -281,9 +332,6 @@ public class Platforms : MonoBehaviour
 		// Returns if no points have been set up
 		if (m_targetPoints.Length == 0)
 			return;
-
-		// Set the agent to go to the currently selected destination.
-		transform.position = Vector3.MoveTowards(transform.position, m_targetPoints[m_nDestPoint].position, m_fSpeed * Time.deltaTime);
 
 		// Choose the next point in the array as the destination,
 		// cycling to the start if necessary.
@@ -321,9 +369,9 @@ public class Platforms : MonoBehaviour
 						GoToNextPoint();
 					}
 				}
-
 			}
-
+			// Set the agent to go to the currently selected destination.
+			transform.position = Vector3.MoveTowards(transform.position, m_targetPoints[m_nDestPoint].position, m_fSpeed * Time.deltaTime);
 		}
 
 	}
