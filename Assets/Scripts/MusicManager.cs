a18273fca3326e9ac9b40cae51c67469ab1c8f1f
Jeremy Zoitas <jezz331@gmail.com>
2018-09-04 15:50:00 +1000

    ----------Some Music components added---------- Added some lines of code needed for audio. added a music manager to play a playlist of music.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MusicManager : MonoBehaviour
+{
+	[SerializeField]
+	private AudioClip[] m_musicClips;
+	[SerializeField]
+	private bool m_bRandomPlay = false;
+
+	private AudioSource m_audioSource;
+	private int m_nCurrentClipIndex = 0;
+
+	// Use this for initialization
+	void Start()
+	{
+		m_audioSource = GetComponent<AudioSource>();
+		m_audioSource.loop = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(!m_audioSource.isPlaying)
+		{
+			AudioClip nextClip;
+			if(m_bRandomPlay)
+			{
+				nextClip = GetRandomClip();
+			}
+			else
+			{
+				nextClip = GetNextClip();
+			}
+			m_nCurrentClipIndex = ArrayUtility.IndexOf(m_musicClips, nextClip);
+			m_audioSource.clip = nextClip;
+			m_audioSource.Play();
+		}
+	}
+
+	private AudioClip GetRandomClip()
+	{
+		return m_musicClips[Random.Range(0, m_musicClips.Length)];
+	}
+
+	private AudioClip GetNextClip()
+	{
+		return m_musicClips[(m_nCurrentClipIndex + 1) % m_musicClips.Length];
+	}
+	private void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+	}
+}
