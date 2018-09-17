b682ff9628b074c20393b31b94d9f98d3432a29f
Jeremy Zoitas <jezz331@gmail.com>
2018-09-18 09:55:58 +1000

    ---Made improvements to transperant sctipt--- reformatted the logic

--- Assets/Scripts/HideObjects.cs
+++ Assets/Scripts/HideObjects.cs
@@ -11,6 +11,7 @@ public class HideObjects : MonoBehaviour
 	public Material HitMaterial;
 	public float duration = 2.0F;
 	public Renderer[] rend;
+	public Renderer[] tempRend;
 	private Dictionary<Transform, Material> _LastTransforms;
 	float lerp = 0.0f;
 	Color color;
@@ -43,7 +44,7 @@ public class HideObjects : MonoBehaviour
 		// Sets the new position for the camera.
 		//transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * m_fDistance, m_fSmooth * Time.deltaTime);
 		UpdateChangeTime();
-		ResetMaterials();
+		//ResetMaterials();
 		RayCastMaterialChange();
 		Debug.DrawRay(m_camera.transform.position, WatchTarget.transform.position - transform.position, Color.green);
 	}
@@ -96,90 +97,92 @@ public class HideObjects : MonoBehaviour
 		  OccluderMask
 		);
 
-		Renderer[] tempRend = new Renderer[rend.Length];
+		tempRend = new Renderer[rend.Length];
 		tempRend = rend;
-		//Debug.DrawRay(transform.position, WatchTarget.transform.position - transform.position);
 		// Loop through all overlapping objects and lerp between materials.
 		// rend element 0 equals the first it object and never changes.
 		if (hits.Length > 0)
 		{
-			// started to attempt a temp renderer array which stores the list of renderers to change to objects back to full opacity if they are not in the array.
 			m_fHitCount = 0;
-			//rend = new Renderer[hits.Length];
+			rend = new Renderer[hits.Length];
 			foreach (RaycastHit hit in hits)
 			{
-				if (hit.collider.gameObject.transform != WatchTarget /*&& hit.collider.transform.root != WatchTarget*/)
+				if (hit.collider.gameObject.transform != WatchTarget)
 				{
 					// lerp equals the change time from the camera collision so it goes from visible to half invisible.
 					lerp = m_fChangeTime;
-					rend = new Renderer[hits.Length];
-					if (hit.collider.gameObject.GetComponent<MeshRenderer>())
-						_LastTransforms.Add(hit.collider.gameObject.transform, hit.collider.gameObject.GetComponent<MeshRenderer>().material);
+					//rend = new Renderer[hits.Length];
+					//if (hit.collider.gameObject.GetComponent<MeshRenderer>())
+						//_LastTransforms.Add(hit.collider.gameObject.transform, hit.collider.gameObject.GetComponent<MeshRenderer>().material);
 					if (hit.collider.gameObject.GetComponent<Renderer>())
 					{
-						for (int i = 0; i <= m_fHitCount; ++i)
+						for (int i = 0; i < rend.Length; ++i)
 						{
-							// Gets the hit gameObjects renderer.
-							rend[i] = hit.collider.gameObject.GetComponent<Renderer>();
-							rend[i].material.SetFloat("_Mode", 3f);
-							rend[i].material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-							rend[i].material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-							rend[i].material.SetInt("_ZWrite", 0);
-							rend[i].material.DisableKeyword("_ALPHATEST_ON");
-							rend[i].material.EnableKeyword("_ALPHABLEND_ON");
-							rend[i].material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-							rend[i].material.renderQueue = 3000;
-							// Lerps between the original material and the hidden one.
-							if (rend[i].material.color.a >= 0.5f)
+							if (hits[i].collider.gameObject == hit.collider.gameObject && hits.Length > 1)
 							{
-								color = rend[i].material.color;
-								color.a -= 0.01f;
-								rend[i].material.color = color;
-							}
-						}
 
-					}
-					for (int i = 0; i < tempRend.Length; ++i)
-					{
-						//if (rend[i] != null)
-						//{
-						if (tempRend[i] != null)
-						{
-							if (i < rend.Length)
-							{
-								if (tempRend[i] == rend[i])
-								{
-								}
 							}
 							else
 							{
-								color = tempRend[i].material.color;
-								color.a = 1.0f;
-								tempRend[i].material.color = color;
-								tempRend[i].material.SetFloat("_Mode", 0f);
-								tempRend[i].material.renderQueue = 2001;
+								// Gets the hit gameObjects renderer.
+								rend[i] = hit.collider.gameObject.GetComponent<Renderer>();
+								rend[i].material.SetFloat("_Mode", 3f);
+								rend[i].material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+								rend[i].material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+								rend[i].material.SetInt("_ZWrite", 0);
+								rend[i].material.DisableKeyword("_ALPHATEST_ON");
+								rend[i].material.EnableKeyword("_ALPHABLEND_ON");
+								rend[i].material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+								rend[i].material.renderQueue = 3000;
+								// Lerps between the original material and the hidden one.
+								if (rend[i].material.color.a >= 0.5f)
+								{
+									color = rend[i].material.color;
+									color.a -= 0.01f;
+									rend[i].material.color = color;
+								}
 							}
 							
 						}
+						for (int i = 0; i < tempRend.Length; ++i)
+						{
+							if (tempRend[i] != null)
+							{
+								if (i < rend.Length)
+								{
+									for(int j = 0; j < rend.Length; ++j)
+									{
+										if (tempRend[i] == rend[j])
+										{
+										}
+									}
+								}
+								else
+								{
+									color = tempRend[i].material.color;
+									color.a = 1.0f;
+									tempRend[i].material.color = color;
+									tempRend[i].material.SetFloat("_Mode", 0f);
+									tempRend[i].material.renderQueue = 2001;
+								}
 
-
-						//}
+							}
+						}
 					}
-					//for (int i = 0; i <= m_fHitCount; ++i)
-					//{
-					//	// had in for loop like the else forgot what happened.
-					//	if (rend[i] && hit.collider.gameObject.GetComponent<MeshRenderer>() != rend[i])
-					//	{
-					//		color = rend[i].material.color;
-					//		color.a = 1.0f;
-					//		rend[i].material.color = color;
-					//		rend[i].material.SetFloat("_Mode", 0f);
-					//		rend[i].material.renderQueue = 2001;
-					//	}
-					//}
-					m_fHitCount++;
 				}
-				//m_fHitCount = hits.Length - 1;
+				//for (int i = 0; i <= m_fHitCount; ++i)
+				//{
+				//	// had in for loop like the else forgot what happened.
+				//	if (rend[i] && hit.collider.gameObject.GetComponent<MeshRenderer>() != rend[i])
+				//	{
+				//		color = rend[i].material.color;
+				//		color.a = 1.0f;
+				//		rend[i].material.color = color;
+				//		rend[i].material.SetFloat("_Mode", 0f);
+				//		rend[i].material.renderQueue = 2001;
+				//	}
+				//}
+				m_fHitCount++;
 			}
 
 		}
