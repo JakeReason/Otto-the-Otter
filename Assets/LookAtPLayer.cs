38fcd6bba47e3b4781fee929ce2940aaf3498fb0
AIE\Max Turner <max.leslie.turner@gmail.com>
2018-09-18 16:42:53 +1000

    the clams will now look at you thanks to jeremy

---
+++ Assets/LookAtPLayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPLayer : MonoBehaviour {
+
+	private GameObject player;
+	public Transform playerTrasform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerTrasform = player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.LookAt (playerTrasform);
+		
+	}
+}
