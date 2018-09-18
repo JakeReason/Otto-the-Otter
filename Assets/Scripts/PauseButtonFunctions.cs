626ff0e2ff6506283b263a6a4e07de00d958834e
Jeremy Zoitas <jezz331@gmail.com>
2018-09-18 13:57:19 +1000

    ---Pause Menu--- Added a quick pause with resume restatr and quit.

---
+++ Assets/Scripts/PauseButtonFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButtonFunctions : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject m_pauseMenu;
+
+	public void Resume()
+	{
+		Time.timeScale = 1;
+		m_pauseMenu.SetActive(false);
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	public void LoadScene(int index)
+	{
+		Time.timeScale = 1;
+		SceneManager.LoadScene(index);
+	}
+
+	public void Quit()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+}
