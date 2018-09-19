c4ecf3b1a227ee24dbb562a13d590c429f3c7f57
AIE\Max Turner <max.leslie.turner@gmail.com>
2018-09-19 12:58:55 +1000

    replaced some of the items in the scen with the new models and textures

---
+++ Assets/PlayAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour {
+
+	public Animator anim;
+	public GameObject path;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim.enabled = false;
+		path.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter (Collider other)
+	{
+		path.SetActive (true);
+		anim.enabled = true;
+
+
+	}
+
+}
