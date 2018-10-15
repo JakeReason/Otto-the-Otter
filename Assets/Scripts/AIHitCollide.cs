a810d31a0cf90d15c0e94573f2b3ec120cf0557d
Jeremy Zoitas <jezz331@gmail.com>
2018-10-15 16:32:31 +1100

    Added ememy flashing when hit

--- Assets/Scripts/AIHitCollide.cs
+++ Assets/Scripts/AIHitCollide.cs
@@ -2,21 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIHitCollide : MonoBehaviour {
+public class AIHitCollide : MonoBehaviour
+{
 
 	public GameObject m_enemy;
+	public GameObject m_enemyModel;
+	public float m_fHitTime;
+	public float m_fFlashingRate;
 	private BasicEnemy m_enemyScript;
+	bool m_bHit;
+	Color m_originalColour;
+	float m_timer;
 
 	// Use this for initialization
-	void Start () {
+	void Awake()
+	{
 		m_enemyScript = m_enemy.GetComponent<BasicEnemy>();
+		m_originalColour = m_enemyModel.GetComponent<SkinnedMeshRenderer>().material.color;
+	}
+	private void Update()
+	{
+		if (m_bHit)
+		{
+			m_timer += Time.deltaTime;
+			if (m_timer <= m_fHitTime)
+			{
+				// Detects if the sine wave value is less than 0
+				if (Mathf.Sin(m_timer / m_fFlashingRate) >= 0)
+				{
+					// Sets Otto's material colour to equal that of the flash colour
+					m_enemyModel.GetComponent<SkinnedMeshRenderer>().material.color = Color.red;
+				}
+				// Otherwise the player's colour is reverted to his initial colour
+				else
+				{
+					m_enemyModel.GetComponent<SkinnedMeshRenderer>().material.color = m_originalColour;
+				}
+			}
+			// Otherwise the player's colour is reverted to his initial colour
+			else
+			{
+				m_enemyModel.GetComponent<SkinnedMeshRenderer>().material.color = m_originalColour;
+				m_timer = 0;
+				m_bHit = false;
+			}
+		}
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if(other.CompareTag("Player"))
+		if (other.CompareTag("Player"))
 		{
 			--m_enemyScript.m_fHealth;
+			m_bHit = true;
 		}
 	}
 }
