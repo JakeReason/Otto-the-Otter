0350c91ec6187c1953795af33f62d472f1fe078a
AIE\Max Turner <max.leslie.turner@gmail.com>
2018-10-15 12:16:33 +1100

    added tree layer

---
+++ Assets/Scripts/EnterTheSnow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTheSnow : MonoBehaviour {
+
+    public GameObject snowDome;
+    public ParticleSystem snow;
+
+
+	// Use this for initialization
+	void Start () {
+
+        snowDome.SetActive(false);
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        snowDome.SetActive(true);
+        snow.Play();
+    }
+
+
+}
