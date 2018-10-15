3da5da390525e82d53194eb36b911b75956d2e85
AIE\Max Turner <max.leslie.turner@gmail.com>
2018-10-15 14:59:36 +1100

    added a track n doly for tutorial level

---
+++ Assets/Tunnel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tunnel : MonoBehaviour {
+
+    public GameObject freeLook;
+    public GameObject trackNDoly;
+
+
+	// Use this for initialization
+	void Start () {
+        trackNDoly.SetActive(false);
+
+
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            trackNDoly.SetActive(true);
+            freeLook.SetActive(false);
+
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            trackNDoly.SetActive(false);
+            freeLook.SetActive(true);
+
+        }
+    }
+}
