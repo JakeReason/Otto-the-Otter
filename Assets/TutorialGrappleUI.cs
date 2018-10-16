f2c9ad39a0b49d08a88bd73eeaac5ebcdc57600a
Max <max.leslie.turner@gmail.com>
2018-10-16 22:58:23 +1100

    Set up the tutrial level all the way up to the flower (Placeholder really)

---
+++ Assets/TutorialGrappleUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialGrappleUI : MonoBehaviour {
+    public GameObject grappleMe;
+
+	// Use this for initialization
+	void Start () {
+        grappleMe.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "HookDetector")
+        {
+            grappleMe.SetActive(true);
+        }
+
+
+    }
+
+    private void OnTriggerExit (Collider other)
+    {
+        if(other.tag == "HookDetector")
+        {
+            grappleMe.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/TutorialUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialUI : MonoBehaviour {
+    public GameObject helpUI;
+
+
+    private void Start()
+    {
+        helpUI.SetActive(false);
+    }
+
+    private void OnTriggerEnter()
+    {
+        helpUI.SetActive(true);
+    }
+
+    private void OnTriggerExit()
+    {
+        helpUI.SetActive(false);
+    }
+
+
+}
