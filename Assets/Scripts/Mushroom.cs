554425d6b8bd5804ac9d894f9bfbc3f6d213a2f5
Matthew Le Nepveu <matthew@lenepveu.com>
2018-10-16 14:03:38 +1100

    Added code to make the mushroom play an animation when the player collides with it, as well as added a bounce sound.

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -9,11 +9,21 @@ public class Mushroom : MonoBehaviour
 	public LayerMask m_playerLayer;
 
 	public GameObject m_player;
+
 	private Player m_playerScript;
+
+	private Animator m_animator;
 
 	// Use this for initialization
 	void Awake()
 	{
+		m_animator = GetComponent<Animator>();
+
+		if (!m_animator)
+		{
+			Debug.Log("No Animator on Mushroom!");
+		}
+
 		m_playerScript = m_player.GetComponent<Player>();
 
 		if (!m_playerScript)
@@ -27,7 +37,6 @@ public class Mushroom : MonoBehaviour
 		if (other.CompareTag("Player"))
 		{
 			m_playerScript.Bounce(m_fBounceForce);
-			//Debug.Log("BOUNCE!");
 		}
 	}
 
