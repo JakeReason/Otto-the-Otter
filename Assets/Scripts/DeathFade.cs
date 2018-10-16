0ed05e3c71fdb988b02f7ed14fd4cd12daf787b9
Jeremy Zoitas <jezz331@gmail.com>
2018-10-16 12:34:01 +1100

    Added test death fade

---
+++ Assets/Scripts/DeathFade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathFade : MonoBehaviour
+{
+
+	public float alpha = 0;
+	Color col;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		col = new Color(GetComponent<Image>().color.r, GetComponent<Image>().color.g, GetComponent<Image>().color.b, alpha);
+		GetComponent<Image>().color = col;
+	}
+}
