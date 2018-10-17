5868529b749ab566aecf36292234e936e249135a
AIE\Max Turner <max.leslie.turner@gmail.com>
2018-10-17 11:32:39 +1100

    updated the player prefab and started to add some of the UI elemenst to the tutorial level

---
+++ Assets/CameraTUT.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTUT : MonoBehaviour {
+    public GameObject gameUI;
+    public GameObject cameraUI;
+    public GameObject m_player;
+
+    private Player m_playerScript;
+
+    private void Awake()
+    {
+        m_playerScript = m_player.GetComponent<Player>();
+        m_playerScript.enabled = false;
+        gameUI.SetActive(false);
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Skip"))
+        {
+            gameUI.SetActive(true);
+            m_playerScript.enabled = true;
+            cameraUI.SetActive(false);
+        }
+    }
+
+
+
+}
