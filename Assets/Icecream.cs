db379b5ec9e05437e5c68199af8e5aa242619dca
AIE\Max Turner <max.leslie.turner@gmail.com>
2018-10-23 16:05:18 +1100

    added a ICE CREAM hidden pickup

---
+++ Assets/Icecream.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icecream : MonoBehaviour {
+    public GameObject iceCreamUI;
+    public AudioSource audioSource;
+    public AudioClip pickUp;
+    public int speed = 1;
+    private bool isPickedup;
+
+	// Use this for initialization
+	void Start () {
+        iceCreamUI.SetActive(false);
+		
+	}
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.down * speed * Time.deltaTime, Space.World);
+
+
+
+        if (audioSource)
+        {
+            if(!audioSource.isPlaying && isPickedup)
+            {
+                gameObject.SetActive(false);
+            }
+            else if (audioSource.isPlaying && isPickedup)
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+                GetComponent<Collider>().enabled = false;
+                transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            iceCreamUI.SetActive(true);
+            audioSource.Play();
+            isPickedup = true;
+        }
+    }
+}
