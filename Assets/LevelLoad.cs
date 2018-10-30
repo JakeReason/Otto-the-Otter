ddfc858e4c7792f2df3d262ed8b9b7e276bbe608
AIE\Max Turner <max.leslie.turner@gmail.com>
2018-10-30 17:02:01 +1100

    added a level select page

---
+++ Assets/LevelLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoad : MonoBehaviour {
+    public GameObject game_Manager;
+    private LevelSelect gameScript;
+
+    private void Awake()
+    {
+        gameScript = game_Manager.GetComponent<LevelSelect>();
+    }
+
+    void LoadNow()
+    {
+        gameScript.loadLevel();
+    }
+}
---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+    public int level1 = 0;
+    public Animator canvus;
+
+
+    public void loadLevel()
+    {
+        if (level1 == 1)
+        {
+            SceneManager.LoadScene("Greybox (Tutoral)");
+        }
+        else if (level1 == 2)
+        {
+            SceneManager.LoadScene("Level 1 Test");
+        }
+    }
+
+    public void tut()
+    {
+        level1 = 1;
+        canvus.SetBool("Fade", true);
+    }
+
+    public void level1Load()
+    {
+        level1 = 2;
+        canvus.SetBool("Fade", true);
+    }
+
+
+    
+}
