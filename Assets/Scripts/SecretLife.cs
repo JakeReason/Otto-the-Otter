d32cc295a6d3ea904dc2db0993afef5dd4c665f2
AIE\Max Turner <max.leslie.turner@gmail.com>
2018-11-05 16:46:28 +1100

    added the new post effect on the main

---
+++ Assets/Scripts/SecretLife.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretLife : MonoBehaviour {
+
+    public GameObject secretPath;
+
+
+	// Use this for initialization
+	void Awake () {
+        secretPath.SetActive(false);
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(SecretStart());
+        }
+    }
+
+
+    IEnumerator SecretStart()
+    {
+        yield return new WaitForSeconds(2f);
+        secretPath.SetActive(true);
+    }
+
+}
